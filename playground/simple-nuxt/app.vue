7dfe39484303d75d302da915666211b5416efb76
Alim ÖZDEMİR <alm.ozdmr@gmail.com>
2024-10-30 20:36:05 +0100

    Playground project added

---
+++ playground/simple-nuxt/app.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <MyComponent></MyComponent>
+    <Head></Head>
+    <NuxtWelcome />
+  </div>
+</template>
